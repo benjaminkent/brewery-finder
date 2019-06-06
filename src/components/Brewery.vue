e352556b469889237e1b7af588ed0dcefc87f665
benjaminkent <benjamin.k.jehl@gmail.com>
2019-06-05 23:45:30 -0400

    map hooked up

--- src/components/Brewery.vue
+++ src/components/Brewery.vue
@@ -3,6 +3,20 @@
     router-link(to="/") Home
     h1 {{ brewery.name }}
     p Brewery Type: {{ brewery.brewery_type }}
+    .map-container(v-if="brewery.latitude")
+      GmapMap(
+        :center="breweryLocation"
+        :zoom="15"
+        style="width: 90%; height: 300px"
+      )
+        GmapMarker(
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+          @click="center=m.position"
+        )
     p {{ brewery.street }}
     p {{ brewery.city }}, {{ brewery.state }}
     p(v-if="brewery.phone") Phone: {{ brewery.phone }}
@@ -16,13 +30,26 @@ export default {
   name: 'brewery',
   data () {
     return {
-      brewery: {}
+      brewery: {},
+      breweryLocation: {},
+      markers: [],
     }
   },
   mounted () {
-    axios
-      .get(`https://api.openbrewerydb.org/breweries/${this.$route.params.breweryId}`)
-      .then(resp => this.brewery = resp.data)
+    this.fetchBrewery()
+  },
+  methods: {
+    fetchBrewery () {
+      axios
+        .get(`https://api.openbrewerydb.org/breweries/${this.$route.params.breweryId}`)
+        .then(resp => {
+          let lat = parseFloat(resp.data.latitude)
+          let lng = parseFloat(resp.data.longitude)
+          this.brewery = resp.data,
+          this.breweryLocation = { lat: lat, lng: lng }
+          this.markers.push({ position: {lat: lat, lng: lng} })
+        })
+    }
   }
 }
 </script>
@@ -39,6 +66,14 @@ export default {
 
 a {
   color: $yellow;
+}
+
+h1 {
+  margin: 0;
+}
+
+p {
+  margin: 0;
 }
 </style>
 
