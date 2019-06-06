305c2a5e71594791a435bf35926146390599b946
benjaminkent <benjamin.k.jehl@gmail.com>
2019-06-05 23:00:34 -0400

    set up main for goog maps

--- src/main.js
+++ src/main.js
@@ -4,10 +4,17 @@ import router from './router'
 import store from './store'
 import '@fortawesome/fontawesome-pro/css/all.css';
 import VueScrollTo from 'vue-scrollto'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
 
 Vue.use(VueScrollTo)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDlFqQ9mdP062pc_dGksw-EI6ZHoiYQGPM',
+    libraries: 'places'
+  }
+})
 
 new Vue({
   router,
